60a3b4912d3ffd80b914eee8e19e34d0dd89436e
Hancil Sequeira <hancil.sequeira@razorpay.com>
2021-06-18 11:29:14 +0530

    Displaying razorpay as 2 payment method for subscription and normal payment

--- view/frontend/web/js/view/payment/razorpay-subscription-payments.js
+++ view/frontend/web/js/view/payment/razorpay-subscription-payments.js
@@ -5,11 +5,16 @@ define(
     ],
     function (Component, rendererList) {
         'use strict';
-        rendererList.push({
-            type: 'razorpay',
-            // component: 'Razorpay_Magento/js/view/payment/method-renderer/razorpay-method'
-            component: 'Razorpay_Subscription/js/view/payment/method-renderer/razorpay-subscription-method'
-        });
+        rendererList.push(
+            {
+                type: 'razorpay',
+                component: 'Razorpay_Magento/js/view/payment/method-renderer/razorpay-method'
+            },
+            {
+                type: 'razorpay_subscription',
+                component: 'Razorpay_Subscription/js/view/payment/method-renderer/razorpay-subscription-method'
+            }
+        );
         /** Add view logic here if needed */
         return Component.extend({});
     }
