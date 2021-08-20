a2e4f23a1cc80b458881e6f8cf9fc1ee46f400a7
Hancil Sequeira <hancil.sequeira@razorpay.com>
2021-08-20 19:32:13 +0530

    Fetching subscription entity id for subscription order details
    Signed-off-by: Hancil Sequeira <hancil.sequeira@razorpay.com>

--- Controller/Adminhtml/OrderDetails/SubscriptionOrderDetails.php
+++ Controller/Adminhtml/OrderDetails/SubscriptionOrderDetails.php
@@ -22,23 +22,19 @@ class SubscriptionOrderDetails extends Template
     {
         parent::__construct($context, $data);
         $this->objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-
-
     }
 
 
     public function fetchSubscriptionOrderDetails()
     {
-
         $order = $this->objectManager->get('\Magento\Sales\Model\Order')->load($this->getRequest()->getParam('order_id'));
 
         /* @var \Magento\Sales\Model\Order $order */
-
         $resource = $this->objectManager->get('Magento\Framework\App\ResourceConnection');
         $connection = $resource->getConnection();
 
         $tableName = $resource->getTableName('razorpay_subscriptions_order_mapping_details');
-        $sql = "SELECT  sub.subscription_id, p.plan_type FROM " . $tableName
+        $sql = "SELECT  sub.entity_id, sub.subscription_id, p.plan_type FROM " . $tableName
             . " AS somd JOIN razorpay_subscriptions AS sub ON somd.subscription_entity_id = sub.entity_id"
             . " JOIN razorpay_plans AS p ON sub.plan_entity_id = p.entity_id WHERE somd.increment_order_id = ".$order->getIncrementId();
         $result = $connection->query($sql);
