b618fb7b6150c1c01003bc6f841453592d0cc1ad
sofiya02 <sofiya.shaikh@razorpay.com>
2022-01-25 17:38:17 +0530

    format and chnages as per magento marketplace report

--- Block/Adminhtml/Subscription/Edit/Form.php
+++ Block/Adminhtml/Subscription/Edit/Form.php
@@ -1,22 +1,22 @@
 <?php
 namespace Razorpay\Subscription\Block\Adminhtml\Subscription\Edit;
-
+ 
 use \Magento\Backend\Block\Widget\Form\Generic;
-
+ 
 class Form extends Generic
 {
-
+ 
     /**
      * @var \Magento\Store\Model\System\Store
      */
     protected $_systemStore;
-
+ 
     /**
      * @param \Magento\Backend\Block\Template\Context $context
-     * @param \Magento\Framework\Registry $registry
-     * @param \Magento\Framework\Data\FormFactory $formFactory
-     * @param \Magento\Store\Model\System\Store $systemStore
-     * @param array $data
+     * @param \Magento\Framework\Registry             $registry
+     * @param \Magento\Framework\Data\FormFactory     $formFactory
+     * @param \Magento\Store\Model\System\Store       $systemStore
+     * @param array                                   $data
      */
     public function __construct(
         \Magento\Backend\Block\Template\Context $context,
@@ -28,7 +28,7 @@ class Form extends Generic
         $this->_systemStore = $systemStore;
         parent::__construct($context, $registry, $formFactory, $data);
     }
-
+ 
     /**
      * Init form
      *
@@ -40,7 +40,7 @@ class Form extends Generic
         $this->setId('subscription_form');
         $this->setTitle(__('Subscription Information'));
     }
-
+ 
     /**
      * Prepare form
      *
